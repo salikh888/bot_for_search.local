872a2da8868fc43f2301fa81528ff0f7661f6933
salik <salikh.sayfutdinov@mail.com>
2021-06-21 23:40:53 +0500

    Добавил axios и переписал cheerio

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,74 @@
-import {BotInit, init} from './bot/bot';
+// import {BotInit, init} from './bot/bot';
+//
+// (async function main() {
+//     new BotInit();
+//     console.log('bot ready');
+// })();
+//
+//
 
+import * as cheerio from 'cheerio';
+import axios from 'axios';
+// import {Config} from '../config';
+// import {BotInit} from "../bot/bot";
+//
+// export function queryMaker(message: string) {
+//     const item = message.replace('/s', '-');
+//     const URL = 'https://www.olx.uz/list/q-' + item;
+//     console.log(URL)
+//     const URLen = encodeURI(URL);
+//     const optionsRequest = {
+//         uri: URL,
+//         headers: {
+//             'User-Agent': Config.HEADERS
+//         },
+//     };
+//     return optionsRequest;
+// }
+
+
+async function getHtml(item: any) {
+    const { data }  =  await axios.get('https://www.olx.uz/list/q-' + item);
+    return data;
+}
+
+async function cheerioLoad(item: any){
+    const $ = cheerio.load(await getHtml(item));
+    return $;
+}
+
+
+function getValuePage(link:any){
+    const arrayMatch: any = link.match('\\d{1,}$');
+    return arrayMatch[0];
+}
+
+async function getLinksItems(item: string){
+    let arrayLink: Array<string | any>;
+    let $ = await cheerioLoad(item);
+    const lastPageNumber = getValuePage($('a.block').eq(-1).attr('href'));
+    for (let i = 1; i <= lastPageNumber; i++){
+        const selector = await cheerioLoad( `${item}/?page=${i}`);
+        selector('.wrap').each((i, element) => {
+            const link = selector(element).find('a.marginright5').attr('href')
+            arrayLink.push(link);
+            return arrayLink;
+        })
+        // @ts-ignore
+
+    }
+
+}
+
+
+
+//_____________________________________________
+async function test() {
+    const  data   = await  getLinksItems('ps3')
+    return data
+}
 (async function main() {
-    new BotInit();
-    console.log('bot ready');
-})();
+     console.log(await test())
 
+})();
 
--- src/parser/script.ts
+++ src/parser/script.ts
@@ -1,7 +1,7 @@
 import * as cheerio from 'cheerio';
-import Request  from 'request-promise';
+import axios from 'axios';
 import {Config} from '../config';
-import {BotInit} from "../bot/bot";
+// import {BotInit} from "../bot/bot";
 
 export function queryMaker(message: string) {
     const item = message.replace('/s', '-');
@@ -9,7 +9,7 @@ export function queryMaker(message: string) {
     console.log(URL)
     const URLen = encodeURI(URL);
     const optionsRequest = {
-        uri : URLen,
+        uri: URL,
         headers: {
             'User-Agent': Config.HEADERS
         },
@@ -17,35 +17,54 @@ export function queryMaker(message: string) {
     return optionsRequest;
 }
 
-export async function setHtml(options: any){
-    let html = await Request(options);
-    //console.log(html)
+
+async function getHtml(item: any) {
+    let html = await axios.get('https://www.olx.uz/list/q-' + item);
+
     return html;
 }
 
-export function search(body: any) {
-    let metadata;
-    let $ = cheerio.load(body);
-    console.log($, '-------------------------')
-    $('div.listContainer')
-        .each(  (i, element) => {
-            console.log($)
+function parser(html: any) {
+    let $ = cheerio.load(html);
 
-            let a = $(search(body)).prev();
-            console.log(a)
-            let url = a.attr('data-url');
-            console.log(url, '-------------------------')
-            metadata.push(url);
-        });
-    return metadata;
+    return $;
 }
 
-export async function main(message: any) {
-    let options = queryMaker(message)
-    let html = await setHtml(options);
-    let array = search(html);
-    return array;
+function pageNumbers($: any) {
+
+    const pageNumbers = $('span.item fleft').eq(-2).text();
+    return pageNumbers;
 }
-(async function yy() {
-    console.log(await main('ps3'));
-})();
+
+function test() {
+    console.log(pageNumbers(parser(getHtml('ps3'))))
+}
+
+test()
+
+// export function search(html: any) {
+//     let metadata;
+//     let $ = cheerio.load(html);
+//     console.log($.html(), '-------------------------')
+//     $('h3.lheight22 margintop5')
+//         .each(  (i, element) => {
+//             console.log($)
+//
+//             let a = $(search(html)).prev();
+//             console.log(a)
+//             let url = a.attr('href');
+//             console.log(url, '-------------------------')
+//             metadata.push(url);
+//         });
+//     return metadata;
+// }
+//
+// export async function main(message: any) {
+//     let options = queryMaker(message)
+//     let html = await getHtml(options);
+//     let array = search(html);
+//     return array;
+// }
+// (async function yy() {
+//     console.log(await main('ps3'));
+// })();
